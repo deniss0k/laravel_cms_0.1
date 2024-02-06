5256402cd3e6db75e9dc1eec6d9bc6aa8bbc9669
deniss0k <deniss.okunevs@esteriol.lv>
2024-02-06 12:46:30 +0200

    public folder fix

--- laravel_cms_0.1/resources/views/adm/dashboard.blade.php
+++ laravel_cms_0.1/resources/views/adm/dashboard.blade.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="format-detection" content="telephone=no">
     <link href="/" rel="shortcut icon" type="image/x-icon"/>
+    <link href="/public/assets/adm/assets/style-BqKA36gt.css" rel="shortcut icon" type="image/x-icon"/>
 </head>
 
 <body>
--- laravel_cms_0.1/vite.config.js
+++ laravel_cms_0.1/vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/adm',
+        outDir: 'public/assets/adm',
     },
 });
